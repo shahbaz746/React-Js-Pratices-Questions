e9f292f53034065640f44e7989af44ff6db82813
Shahbaz <shahbazrao445@gmail.com>
2025-08-11 18:38:12 +0500

    props Pratices Questions

---
+++ prop/src/components/Arrys.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Arrys = () => {
+  return (
+    <div>
+      const products = [
+  { id: 1, name: "Shirt", price: 20 },
+  { id: 2, name: "Pants", price: 35 },
+    ];
+    </div>
+  )
+}
+
+export default Arrys
